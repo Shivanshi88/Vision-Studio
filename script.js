08aef74e67b26f1df7afb609d06f2380dcec171e
Shivanshi Sahu <Shivanshisahu88@gmail.com>
2025-04-29 20:27:23 +0530

    page 1 work done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+function loadingAnimation(){
+   
 var tl=gsap.timeline();
 tl.from(".line h1",{
     y:150,
@@ -30,16 +31,38 @@ opacity:1,
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4,
+    delay:3.5,
 });
 tl.from("#page1",{
 delay:0.2,
 y:1600,
+duration:0.6,
 opacity:0,
 ease:Power4
 })
 tl.to("#loader",{
 display:"none",
 })
-
-
+tl.from("#nav",{
+    opacity:0,
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2,
+}) 
+}
+loadingAnimation()
+function cursonAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y
+        });
+        })
+        Shery.makeMagnet("#nav-part2 h4", /* Element to target.*/ {
+            //Parameters are optional.
+            ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+            duration: 1,
+          });
+}
+cursonAnimation()
